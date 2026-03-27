cc56539508fd86c4bdd16af40d2e8768cd7fc344
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-03-27 18:11:09 +0100

    städar upp bland filer jag ej kmr använda

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div>
+      <h1>Nathalie</h1>
+    </div>
+  </React.StrictMode>
 )
